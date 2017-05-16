2e1c8fc5617e456ab86bc2ead74c1c820d5e0dfe
KERIL-B <kibessonov@mail.ru>
2017-05-16 21:35:38 +0300

    PieChart for Double

---
+++ UsingPieChart/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UsingPieChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pieChartUC1 = new PieChart.PieChartUC();
+            this.SuspendLayout();
+            // 
+            // pieChartUC1
+            // 
+            this.pieChartUC1.Location = new System.Drawing.Point(42, 28);
+            this.pieChartUC1.Name = "pieChartUC1";
+            this.pieChartUC1.Size = new System.Drawing.Size(202, 202);
+            this.pieChartUC1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pieChartUC1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PieChart.PieChartUC pieChartUC1;
+
+
+
+    }
+}
+
---
+++ UsingPieChart/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingPieChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Random rand = new Random();
+            List<double> list = new List<double>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(rand.Next(20));
+            }
+
+            pieChartUC1.Create(list);
+        }
+    }
+}
